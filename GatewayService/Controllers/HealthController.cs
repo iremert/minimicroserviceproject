fc291ee0616df7818fe0f0d97d49db34ebd7048c
iremertürk <144621839+iremert@users.noreply.github.com>
2025-08-22 16:43:21 +0300

    add Gateway Controller for AWS

---
+++ GatewayService/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GatewayService.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("healthy");
+    }
+}
